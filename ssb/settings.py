ec31c2d009e652a4c3c1f9f4347605b7dcc18d08
Ahmad Afiq Azmi <ahmad_afiq53@yahoo.com>
2022-04-07 14:40:13 +0800

    Update settings.py

--- ssb/settings.py
+++ ssb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["sistemsimulasiblockchain.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["sistemsimulasiblockchain.herokuapp.com"]
 
 
 # Application definition
